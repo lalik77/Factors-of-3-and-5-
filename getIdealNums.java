08615deea6be64c7331f9f5e27060b4c4210f603
Alexander Mami <45859153+lalik77@users.noreply.github.com>
2019-12-14 16:00:49 +0300

    Update getIdealNums.java

--- getIdealNums.java
+++ getIdealNums.java
@@ -1,4 +1,6 @@
- static long getIdealNums(long low, long high) // 3 and 5 as primitive factors
+// First way of algorithm  
+
+static long getIdealNums(long low, long high) // 3 and 5 as primitive factors
     {
         
         int count = 0; // counter 
@@ -22,3 +24,58 @@
 
         return count;
     }
+
+// Second Way of Algorithm
+
+
+static int exp(int base, long n) {
+
+        // 2^3=8 , 2^x=8 -> x = ln8/ln2 , otherwise x=Math.log(8)/Math/log(2)
+
+        return (int) ( Math.log(n)/Math.log(base) );
+    }
+
+    static long  getIdealNumber (long low,long high) {
+
+        // low<= 3^exp3*5^exp5 <= high
+
+        int exp3_Max= exp(3,high);
+        int exp5_Max= exp(5,high);
+
+        System.out.printf("Max exp3 (3^exp3_max) = %d , Max exp5(5^exp5_Max) = %d" , exp3_Max , exp5_Max );
+        System.out.println(" ");
+
+
+        int idealCounter=0;
+
+        long n3=1L;
+
+
+        for (int i=0;i<=exp3_Max;i++) {
+
+            long n5=1l;
+         
+            for (int j=0;j<=exp5_Max;j++) {
+
+                long num = n3*n5;
+                System.out.printf("3^%d * 5^%d = %d%n", i, j, num);
+
+                if (num>high) {
+                    break ;
+                }
+                if (num>=low) {
+                    System.out.println("---> " + num);
+                    idealCounter++;
+                }
+                n5*=5;
+
+            }
+            n3*=3;
+        }
+
+        return idealCounter;
+
+
+
+    }
+
